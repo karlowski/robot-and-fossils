3034f4bf9ce18ca3800b02b0a6d236986cd0c8f4
Stanislav Karlov <karlovvski@gmail.com>
2023-11-02 13:16:19 +0200

    fixed: project specifications

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/robot-game-angular'),
+      dir: require('path').join(__dirname, './coverage/robot-and-fossils'),
       subdir: '.',
       reporters: [
         { type: 'html' },
